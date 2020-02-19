f923dfdeab6c9528b5343030559c292ea6f9c661
jonathan-crepeau <jon.crepeau@gmail.com>
2020-02-19 11:59:38 -0800

    main_app initial urls + views

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('users/<int:user_id>/', views.detail_profile, name='detail_profile'),
+    path('users/<int:user_id>/edit/', views.edit_profile, name='edit_profile'),
+    path('users/<int:user_id>/delete/', views.delete_profile, name='delete_profile'),
+
+    path('users/intro', views.intro, name='intro'),
+    path('users/createprofile', views.create_profile, name='create_profile'),
+    path('users/mainfeed', views.main_feed, name='main_feed'),
+    path('users/feedsearch', views.feed_search, name='feed_search'),
+
 ]
