9a785f44ea6ea054b121b8fd922f9e8a00d5d67f
jonathan-crepeau <jon.crepeau@gmail.com>
2020-02-20 07:29:00 -0800

    displaying register form

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     return HttpResponse('<h1>Accounts Homepage!</h1>')
+
+def register(request):
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form':form})
