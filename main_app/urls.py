cca6b1534fc11755304b6409888f4709378662fd
jonathan-crepeau <jon.crepeau@gmail.com>
2020-02-20 13:49:06 -0800

    profile model creation complete without zipcode

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('users/<int:user_id>/delete/', views.delete_profile, name='delete_profile'),
 
     path('users/intro', views.intro, name='intro'),
-    path('users/createprofile', views.create_profile, name='create_profile'),
+    path('users/createprofile/', views.create_profile, name='create_profile'),
     path('users/mainfeed', views.main_feed, name='main_feed'),
     path('users/feedsearch', views.feed_search, name='feed_search'),
 
