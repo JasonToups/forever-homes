f2b5739cdbdc38110f1cce961f4da52faa28cb45
jonathan-crepeau <jon.crepeau@gmail.com>
2020-02-20 07:39:59 -0800

    success with registration form, checked against admin page

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
@@ -6,5 +6,11 @@ def index(request):
     return HttpResponse('<h1>Accounts Homepage!</h1>')
 
 def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
         form = UserCreationForm()
         return render(request, 'register.html', {'form':form})
