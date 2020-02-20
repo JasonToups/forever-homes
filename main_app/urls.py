1a093da541e40280417ccc3e3625ed94e33873bf
gpastorino <gpastorino@gmail.com>
2020-02-20 14:55:27 -0800

    adds templates and makes some changes to models, urls and views

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path('users/mainfeed', views.main_feed, name='main_feed'),
     path('users/feedsearch', views.feed_search, name='feed_search'),
 
+    # gustav-dev adds: 
+    path('users/logout/', views.logout, name='logout'),
+    # path('users/createaccount', views.create_account, name='create_account'),
+    path('users/logoutsuccess', views.logout_success, name='logout_success'),
+    # path('users/favorites', views.favorites, name='favorites'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
+
+from django.shortcuts import render, redirect
+
+from .forms import LoginForm
+
+from django.contrib
+
 
 # taking a web request and returns a web response.
 # fetch objects from database
@@ -9,6 +17,9 @@ from django.http import HttpResponse
 
 
 def index(request):
+
+
+
     return HttpResponse('<h1>Homepage!</h1>')
 
 def intro(request):
@@ -24,11 +35,58 @@ def main_feed(request):
 def feed_search(request):
     return HttpResponse('<h1>Main Feed Search!</h1>')
 
+
+# I've added this to make sure a request directs to the detail_profile with the necessary elements:
+
+
 def detail_profile(request):
-    return HttpResponse('<h1>Detail Profile View!</h1>')
+    # return HttpResponse('<h1>Detail Profile View!</h1>')
+    return render(request, 'detail_profile.html', {'username': username})
+
 
 def edit_profile(request):
-    return HttpResponse('<h1>Edit Profile!</h1>')
+    # return HttpResponse('<h1>Edit Profile!</h1>')
+    return render(request, 'detail_profile.html', {'username': username})
 
 def delete_profile(request):
-    return HttpResponse('<h1>Delete Profile!</h1>')
+    # return HttpResponse('<h1>Delete Profile!</h1>')
+    return render(request, 'detail_profile.html', {'username': username})
+
+def logout(request):
+    logout(request)
+    return render(request, 'logout_success.html')
+
+
+def logout_success(request):
+        logout_success(request)
+        return redirect('/')
+
+
+
+# from cat-collectr
+def login_view(request):
+        if request.method == 'POST':
+                form = LoginForm(request.POST)
+                if form.is_valid():
+                        u = form.cleaned_data['username']
+                        p = form.cleaned_data['password']
+                        user = authenticate(username = u, password = p)
+                        if user. is_active:
+                                login(request, user)
+                                return redirect('/')
+                        else:
+                                print("The account has been disabled.")
+                else:
+                        print("the username and/or password is incorrect.")
+        else: 
+                form = LoginForm()
+                return render(request, 'login.html', {'form': form})
+
+# from cat-collectr
+def profile(request, username):
+        user = User.objects.get(username=username)
+
+        # cats= Cat.objects.filter(user=user)
+        return render(request, 'profile.html', {'username': username})
+        # return render(request, 'profile.html', {'username': username, 'cats': cats})
+
