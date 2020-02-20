c14b8ac3263bf545743ad3f6d37da58157f399d5
jason-toups <me@jasontoups.com>
2020-02-20 15:01:59 -0800

    adds hamburger menu to nav

--- main_app/static/js/api.js
+++ main_app/static/js/api.js
@@ -187,5 +187,17 @@ const onError = (error, errorText, errorCode) => {
   console.log({ error })
 };
 
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+myFunction()
+
 /* This invokes the function to get the token, which starts the series of API requests */
 getToken();
