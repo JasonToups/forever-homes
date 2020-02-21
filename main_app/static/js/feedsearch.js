5e0b7cc3785f8d807d43d8d4f2bdc50bc0d2c5e2
jonathan-crepeau <jon.crepeau@gmail.com>
2020-02-21 12:49:52 -0800

    search terms displaying, need to update uncheck functionalty and add submit

--- main_app/static/js/feedsearch.js
+++ main_app/static/js/feedsearch.js
@@ -1,15 +1,22 @@
 console.log('Feedsearch ready to go!')
 
-// var limit = 3;
-// $('input.single-checkbox').on('change', function(evt) {
-//    if($(this).siblings(':checked').length >= limit) {
-//        this.checked = false;
-//    }
-// });
-
-var limit = 1;
+let limit = 1;
 $('.checked-box').on('change', function(evt) {
         if($(this).siblings(':checked').length >= limit) {
+                this.checked = false;
+        }
+});
+
+let coatlimit = 1;
+$('#searchCoats').on('change', '.coats-box', function (evt) {
+        if ($(this).siblings(':checked').length >= coatlimit) {
+                this.checked = false;
+        }
+});
+
+let colorLimit=1;
+$('#searchColors').on('change', '.colors-box', function(evt) {
+        if ($(this).siblings(':checked').length >= colorLimit) {
                 this.checked = false;
         }
 });
@@ -17,11 +24,8 @@ $('.checked-box').on('change', function(evt) {
 let apiResponse;
 
 $('.checked-box').on('click', function(evt) {
-
-        let userType= $(this).val();
+        let userType= encodeURIComponent($(this).val());
         console.log(userType);
-
-
         $.ajax({
                 url: `https://api.petfinder.com/v2/types/${userType}/`,
                 method: 'GET',
@@ -34,11 +38,48 @@ $('.checked-box').on('click', function(evt) {
                 processData: false,
                 success: onSuccess, 
                 error: error => console.log(error)
-
               });
 });
 
 const onSuccess = response => {
         apiResponse = response;
+        // console.log(apiResponse)
+        populateCoats();
 };
-console.log(apiResponse);
+
+function populateCoats() {
+        if (apiResponse.type.coats.length !== 0) {
+                $('#searchCoats').append('<br/><h3>Select A Single Type Of Coat<h3>')
+                for (let i=0; i<apiResponse.type.coats.length; i++) {
+                        $('#searchCoats').append(`
+                        <input class="coats-box" type="checkbox" name="type1" value="${apiResponse.type.coats[i]}">
+                                <label for="type1">${apiResponse.type.coats[i]}</label><br>
+                        `)
+                }
+        }
+        populateColors();
+};
+
+function populateColors() {
+        if (apiResponse.type.colors !== 0) {
+                $('#searchColors').append('<br/><h3>Select A Single Color</h3>')
+                for (let i=0; i<apiResponse.type.colors.length; i++) {
+                        $('#searchColors').append(`
+                        <input class="colors-box" type="checkbox" name="type1" value="${apiResponse.type.colors[i]}">
+                                <label for="type1">${apiResponse.type.colors[i]}</label><br>
+                        `)
+                }
+        }
+        populateGender();
+}
+
+function populateGender() {
+        $('#searchGenders').append('<br/><h3>Select A Single Gender</h3>')
+        for (let i = 0; i < apiResponse.type.genders.length; i++) {
+                $('#searchGenders').append(`
+                        <input class="colors-box" type="checkbox" name="type1" value="${apiResponse.type.genders[i]}">
+                                <label for="type1">${apiResponse.type.genders[i]}</label><br>
+                        `)
+        }
+}
+
