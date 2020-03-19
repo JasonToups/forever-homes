fbe4a0ded217683126dd2dc8a49c436f3562472c
jason-toups <me@jasontoups.com>
2020-03-18 20:53:04 -0700

    updates static url

--- django_foreverhomes/settings.py
+++ django_foreverhomes/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '../main_app/static/'
 
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static')),
 
